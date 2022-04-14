3bac474bb69db1cd654bad8ae4dbf66e9196a59a
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2022-04-14 17:05:12 +0600

    image pick and upload to firebase.

--- app/src/main/java/com/sabikrahat/studyzone/EditProfile.java
+++ app/src/main/java/com/sabikrahat/studyzone/EditProfile.java
@@ -1,29 +1,43 @@
 package com.sabikrahat.studyzone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.sabikrahat.studyzone.models.User;
 
 import java.util.HashMap;
@@ -36,11 +50,17 @@ public class EditProfile extends AppCompatActivity {
     private TextView editPic;
     private EditText name, phone;
     private Button save;
+    private ProgressDialog progressDialog;
+    private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
-    private String RID = "";
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    private StorageReference mStorageRef;
+
+    private static final int IMAGE_PICK_CODE = 1;
+    private Uri mImageUri;
+    private String downloadImageUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,15 +75,16 @@ public class EditProfile extends AppCompatActivity {
         name = findViewById(R.id.fullname);
         phone = findViewById(R.id.phone_number);
         save = findViewById(R.id.save_changes);
+        progressBar = findViewById(R.id.progressBar);
 
         mAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid());
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User user = snapshot.getValue(User.class);
-                RID = user.getRid();
                 try {
                     Glide.with(EditProfile.this).load(user.getImageURL()).into(profilePic);
                 } catch (Exception e) {
@@ -79,77 +100,94 @@ public class EditProfile extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.tv_change).setOnClickListener(view -> imagePickAndProcess());
+        findViewById(R.id.tv_change).setOnClickListener(view -> pickImage());
+        profilePic.setOnClickListener(view -> pickImage());
 
         save.setOnClickListener(v -> saveData());
     }
-    
+
     public void saveData() {
-        DatabaseReference databaseReference1 = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid());
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Saving Changes...");
+        progressDialog.show();
 
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("name", name.getText().toString());
-        hashMap.put("phone", phone.getText().toString());
-
-        databaseReference1.updateChildren(hashMap);
-
-//                DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("Rid").child(RID);
-//
-//                HashMap<String, Object> hashMap_R = new HashMap<>();
-//                hashMap_R.put("name", name.getText().toString());
-//                hashMap_R.put("phoneNumber", phone.getText().toString());
-//
-//                databaseRef.updateChildren(hashMap_R);
+        if (mImageUri != null) {
+           uploadFile();
+        } else {
+            DatabaseReference databaseReference1 = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid());
+            HashMap<String, Object> hashMap = new HashMap<>();
+            hashMap.put("name", name.getText().toString());
+            hashMap.put("phone", phone.getText().toString());
+            databaseReference1.updateChildren(hashMap);
+        }
+        progressDialog.dismiss();
         Toast.makeText(EditProfile.this, "Data updated successfully.", Toast.LENGTH_SHORT).show();
-
         finish();
         startActivity(new Intent(EditProfile.this, MainActivity.class));
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void imagePickAndProcess() {
-        boolean pick = true;
-        if (pick) {
-            if (!checkCameraPermission()) {
-                System.out.println("Camera permission not granted");
-                requestCameraPermission();
-            } else {
-                pickImage();
-            }
+    private void pickImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_PICK_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Glide.with(EditProfile.this).load(mImageUri).into(profilePic);
         } else {
-            if (!checkStoragePermission()) {
-                requestStoragePermission();
-            } else {
-                pickImage();
-            }
+            Toast.makeText(this, "No image selected.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private boolean checkCameraPermission() {
-        boolean cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-        boolean writeStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-        System.out.println("Camera permission: " + cameraPermission);
-        return cameraPermission && writeStoragePermission;
+    private void uploadFile() {
+        if (mImageUri != null) {
+            System.out.println("Download URL:  Image Uri: " + mImageUri);
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mImageUri));
+
+            fileReference.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Handler handler = new Handler();
+                    handler.postDelayed(() -> {
+                        progressBar.setProgress(0);
+                    }, 5000);
+
+                    Toast.makeText(EditProfile.this, "Upload successful.", Toast.LENGTH_LONG).show();
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful()) ;
+                    Uri uri = urlTask.getResult();
+                    String downloadImageUrl= uri.toString();
+                    System.out.println("Download URL:  " + downloadImageUrl);
+
+                    DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid());
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("imageURL", downloadImageUrl);
+                    databaseRef.updateChildren(hashMap);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EditProfile.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                    double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                    progressBar.setProgress((int) progress);
+                }
+            });
+        }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void requestCameraPermission() {
-        System.out.println("Requesting camera permission");
-        requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
-    }
-
-    private boolean checkStoragePermission() {
-        System.out.println("Checking storage permission");
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void requestStoragePermission() {
-        System.out.println("Requesting storage permission");
-        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
-    }
-
-    private void pickImage() {
-        Toast.makeText(this, "Will updated later.", Toast.LENGTH_SHORT).show();
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 }
